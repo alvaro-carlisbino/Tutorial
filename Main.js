cb6d304e6516c7da8073da73c80346bc7ac9027b
Alvaro Matheus <xdbrgames10@gmail.com>
2021-01-30 11:46:47 -0300

    Segunda aula

--- Main.js
+++ Main.js
@@ -1,9 +1,24 @@
 const discord = require("discord.js")
-const client = new discord.Client({ws: {intents: ["GUILDS", "GUILD_MEMBERS"]}})
+const client = new discord.Client()
 const config = require("./config.json")
 
 client.login(config.token)
 
 client.on("ready", () => {
   console.log(`${client.user.username} LOGADO COM SUCESSO`)
+})
+
+let prefix = "."
+
+client.on("message", async(message) => {
+  let author = message.author;
+  let guild = message.guild;
+  let channel = message.channel;
+
+  if(author.bot)return;
+  if(channel.type == "dm") return;
+
+  if(message.content.startsWith(prefix+"ping")){
+    channel.send(`${Math.floor(client.ws.ping)}ms`)
+  }
 })
